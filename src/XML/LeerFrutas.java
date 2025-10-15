0db1e3ce6018588f603e818657d9d5b4a6a53b83
pblovs <pablovise04@gmail.com>
2025-10-15 19:22:35 +0200

    Filtros añadidos

--- src/XML/LeerFrutas.java
+++ src/XML/LeerFrutas.java
@@ -3,6 +3,7 @@ package XML;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,9 +14,51 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class LeerFrutas {
+	
+	public static void filtrarPorPrecio(Scanner sc, List<Fruta> frutas) {
+		System.out.println("Filtrar por precio, > (1), < (2), = (3): ");
+		int opcion = sc.nextInt();
+		System.out.println("Introduce el precio: ");
+		double precioFiltrar = sc.nextDouble();
+		for (Fruta f : frutas) {
+			switch(opcion) {
+			case 1: 
+				if (precioFiltrar < Double.parseDouble(f.precio)) {
+					System.out.println(f.toString());
+				}
+				break;
+			case 2: 
+				if (precioFiltrar > Double.parseDouble(f.precio)) {
+					System.out.println(f.toString());
+				}
+				break;
+			case 3: 
+				if (precioFiltrar == Double.parseDouble(f.precio)) {
+					System.out.println(f.toString());
+				}
+				break;
+			default:
+                System.out.println("Opción no válida");
+                break;
+			}
+		}
+	}
+	
+	public static void filtrarPorNutriente(Scanner sc, List<Fruta> frutas) {
+		System.out.println("Introduce el nutriente: ");
+		sc.nextLine();
+		String nutri = sc.nextLine();
+		for (Fruta f : frutas) {
+			if (f.nutrientes.contains(nutri)) {
+				System.out.println(f.toString());
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
+		
 		try {
 			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance ( );
@@ -55,13 +98,18 @@ public class LeerFrutas {
 		    for (Fruta f : frutas) {
                 System.out.println(f.toString());
             }
+		    
+		    filtrarPorPrecio(sc, frutas);
+		    System.out.println();
+		    filtrarPorNutriente(sc, frutas);
 
 		 }
 		 catch (Exception e)
 		 {
 			 e.printStackTrace();
 		 }
-
+		
+		sc.close();
 	}
 
 }
