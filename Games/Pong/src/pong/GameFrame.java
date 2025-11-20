2dbe4735450b40c27a39c152c9694f4679b161c6
pblovs <pablovise04@gmail.com>
2025-11-20 21:27:15 +0100

    PONG

--- Games/Pong/src/pong/GameFrame.java
+++ Games/Pong/src/pong/GameFrame.java
@@ -1,12 +1,23 @@
 package pong;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 	
-	GamePanel panel = new GamePanel();
+	GamePanel panel;
 	
 	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 		
 	}
 
